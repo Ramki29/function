76fd09a20f4b76712c6de12f30f22ab64ba42727
Ramki29 <ramkie7010@gmail.com>
2023-03-15 09:46:53 +0530

    add function

---
+++ main.js
@@ -0,0 +1,33 @@
+function greet()
+	{
+		var a=10,b=5,c;
+		c=a+b;
+		document.write(c);
+	}
+	greet();
+	document.write("<br>");
+	greet();
+	document.write("<br>");
+	greet();
+	document.write("<br>");
+	
+	
+function good()
+	{
+		var x=12,y=8,z;
+		z=x+y;
+		return z;
+	}
+	let result=good();
+	document.write(result/2);
+	document.write("<br>");
+	
+	
+function add (num1,num2)	
+	{
+	let c=num1+num2;
+	document.write(c);
+	}
+	add(10,20);
+	document.write("<br>");
+	add(20,20);
